98861d46859b549a032b8b81e7b4da04367b852f
Will Robertson <wspr81@gmail.com>
2016-01-27 16:38:02 +0800

    add build.lua and update docs for it

---
+++ build.lua
@@ -0,0 +1,22 @@
+#!/usr/bin/env texlua
+
+module = "fontspec"
+
+sourcefiles  = {"*.dtx","doc-files/*"}
+unpackfiles  = {"fontspec.dtx"}
+installfiles = {"*.sty","fontspec.lua"}
+typesetfiles = {"fontspec.dtx"}
+typesetsuppfiles = {"doc-files/"}
+docfiles     = {"fontspec-doc.tex","doc-files/*"}
+demofiles    = {"fontspec-example.tex"}
+
+checkopts   = " -interaction=errorstopmode -halt-on-error "
+typesetopts = " -shell-escape -interaction=errorstopmode "
+
+checkengines = {"xetex","luatex"}
+typesetexe = "xelatex"
+
+packtdszip = true
+
+kpse.set_program_name("kpsewhich")
+dofile(kpse.lookup("l3build.lua"))

e5239f7e109f01df2bdfc2e24c2ffe656af384a6
Will Robertson <will@wspr.io>
2017-08-06 20:08:19 +0930

    something something cd
    getting tired of this dance

--- texlive.sh
+++ texlive.sh
@@ -55,7 +55,8 @@ ls -la /tmp/fontspec-test-fonts
 # Install custom fontspec fonts
 if [ -d "/tmp/fontspec-test-fonts/.git" ]; then
   cd /tmp/fontspec-test-fonts ;
-  git pull ;
+  git pull --rebase ;
+  cd -;
 else
   git clone https://github.com/wspr/fontspec-test-fonts.git /tmp/fontspec-test-fonts ;
 fi

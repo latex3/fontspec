512e2edf5685e90f654bf1af93fbd0a8b1d15fb7
Will Robertson <wspr81@gmail.com>
2017-11-05 22:37:06 +1030

    update build.lua
    closes #311

--- build.lua
+++ build.lua
@@ -2,10 +2,11 @@
 
 module = "fontspec"
 
-sourcefiles  = {"*.dtx","*.ltx","*.cfg","fontspec-doc*.tex","fontspec-doc-style.sty"}
+sourcefiles  = {"*.ins","*.dtx","*.ltx","*.cfg","*.tex","fontspec-doc-style.sty"}
 installfiles = {"fontspec.sty","fontspec-xetex.sty","fontspec-luatex.sty","fontspec.lua","fontspec.cfg"}
-typesetfiles = {"*.ltx"}
+typesetfiles = {"fontspec.ltx","fontspec-code.ltx"}
 demofiles    = {"fontspec-example.tex"}
+textfiles    = {"README.md","CHANGES.md"}
 
 typesetopts = " -shell-escape -interaction=errorstopmode "
 unpackopts  = " -interaction=batchmode "

74ca65c04644c93f355dcca274bf9030fcc52708
Will Robertson <wspr81@gmail.com>
2019-01-18 00:30:56 +1030

    an experiment in an automated release script
    all fun and games until someone uploads something totally broken

---
+++ autorelease.lua
@@ -0,0 +1,45 @@
+#!/usr/bin/env texlua
+
+function os.capture(cmd, raw)
+  local f = assert(io.popen(cmd, 'r'))
+  local s = assert(f:read('*a'))
+  f:close()
+  if raw then return s end
+  s = string.gsub(s, '^%s+', '')
+  s = string.gsub(s, '%s+$', '')
+  s = string.gsub(s, '[\n\r]+', ' ')
+  return s
+end
+
+function exe(s)
+  local e = os.execute(s)
+  if e > 0 then
+    error("ABORT")
+  end
+end
+
+gitbranch = os.capture('git symbolic-ref --short HEAD')
+print("Current git branch: "..gitbranch)
+if gitbranch ~= "working" then
+  error("You must be on the 'working' branch")
+end
+
+os.execute("git pull")
+os.execute("git push")
+
+travis = os.capture("travis status | grep passed")
+print("Travis status: "..travis)
+
+exe("git checkout master")
+
+exe("git rebase working")
+
+exe("l3build ctan")
+
+exe("texlua tagrelease.lua")
+
+exe("l3build upload --file CHANGES-NEW.md")
+
+exe("git push")
+
+exe("git checkout working")

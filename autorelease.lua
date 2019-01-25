6c5153b45a35d66c15acff99e690795413d7af0a
Will Robertson <wspr81@gmail.com>
2019-01-25 22:37:42 +1030

    add a stage to autorelease

--- autorelease.lua
+++ autorelease.lua
@@ -20,20 +20,54 @@ function exe(s)
   end
 end
 
+function usercheck()
+  print(("Happy? [y/n]")
+  ans = io.read()
+  if not(lower(ans,1,1)=="y") then
+    error("USER ABORTED")
+  end
+end
+
 gitbranch = os.capture('git symbolic-ref --short HEAD')
 print("Current git branch: "..gitbranch)
 if gitbranch ~= "working" then
   error("You must be on the 'working' branch")
 end
 
-travis = os.capture("travis status | grep passed")
-print("Travis status: "..travis)
+--[=========[
+     START
+--]=========]
 
 exe("git checkout master")
-
 exe("git pull")
-
 exe("git rebase working")
+
+travis = os.capture("travis status")
+print("Travis status: "..travis)
+
+changeslisting = nil
+do
+  local f = assert(io.open("CHANGES.md", "r"))
+  changeslisting = f:read("*all")
+  f:close()
+end
+currentchanges = string.match(changeslisting,"(## %S+ %(.-%).-)%s*## %S+ %(.-%)")
+
+print("******************")
+print(currentchanges)
+print("******************")
+
+pkgversion = string.match(currentchanges,"## (%S+) %(.-%)")
+print('New version: '..pkgversion)
+
+print('Current tag:')
+os.execute('git tag --contains | head -n1')
+
+usercheck()
+
+--[============[
+     CONTINUE
+--]============]
 
 exe("l3build tag foo")
 
@@ -50,6 +84,10 @@ exe("l3build ctan")
 exe("texlua tagrelease.lua")
 
 exe("l3build upload --file CHANGES-NEW.md")
+
+--[============[
+       END
+--]============]
 
 exe("rm CHANGES-NEW.md")
 

9bb9e4519aacf0baa877ccceb986513eacaf12be
Will Robertson <wspr81@gmail.com>
2019-04-15 14:28:18 +0930

    oops

--- testsetup/install-texlive.sh
+++ testsetup/install-texlive.sh
@@ -15,7 +15,7 @@ if ! command -v texlua > /dev/null; then
   cd install-tl-20*
 
   # Install a minimal system
-  ./install-tl --profile=testsetup/texlive.profile
+  ./install-tl --profile=../testsetup/texlive.profile
 
   cd ..
 fi
